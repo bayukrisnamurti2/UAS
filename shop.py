d8bf0123d52f141065f58a4ecb936afad8b55d23
bayukrisnamurti2 <148696366+bayukrisnamurti2@users.noreply.github.com>
2023-12-29 20:27:16 +0700

    shop.py

--- shop.py
+++ shop.py
@@ -2,45 +2,29 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 # Membaca dataset dari file CSV
 df = pd.read_csv('Customers.csv')
 
 df = pd.DataFrame(df)
 
-# Menentukan jumlah kelompok (clusters)
-k = 3
-
 # Memilih kolom yang akan digunakan untuk clustering
 selected_columns = ['Age', 'Annual Income ($)', 'Spending Score (1-100)']
 selected_data = df[selected_columns]
 
-# Membuat objek KMeans dengan jumlah kelompok k
-kmeans = KMeans(n_clusters=k)
+# Menentukan jumlah cluster maksimum yang akan diuji
+max_clusters = 10
 
-# Melatih model KMeans dengan data
-kmeans.fit(selected_data)
+# Melakukan iterasi untuk berbagai jumlah cluster dan menghitung inersia
+inertias = []
+for k in range(1, max_clusters + 1):
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(selected_data)
+    inertias.append(kmeans.inertia_)
 
-# Mendapatkan label kelompok untuk setiap data point
-labels = kmeans.labels_
-
-# Menambahkan kolom label ke dalam DataFrame
-df['Cluster'] = labels
-
-# Visualisasi hasil dalam bentuk scatter plot 3D
-fig = plt.figure(figsize=(10, 7))
-ax = fig.add_subplot(111, projection='3d')
-
-colors = ['r', 'g', 'b']
-for i in range(k):
-    cluster_data = selected_data[df['Cluster'] == i]
-    ax.scatter(cluster_data['Age'], cluster_data['Annual Income ($)'], cluster_data['Spending Score (1-100)'],
-               c=colors[i], label=f'Cluster {i + 1}')
-
-ax.set_xlabel('Age')
-ax.set_ylabel('Annual Income ($)')
-ax.set_zlabel('Spending Score (1-100)')
-ax.set_title('K-Means Clustering')
-ax.legend()
+# Memplot grafik elbow
+plt.plot(range(1, max_clusters + 1), inertias, marker='o')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Inertia')
+plt.title('Elbow Method for Optimal Cluster Number')
 plt.show()

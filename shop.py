4755b462f9acd769b937a18eb20212196bea1ed7
bayukrisnamurti2 <148696366+bayukrisnamurti2@users.noreply.github.com>
2023-12-29 20:22:13 +0700

    shop.py

--- shop.py
+++ shop.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Membaca dataset dari file CSV
-df = pd.read_csv('C:\Customers.csv')
+df = pd.read_csv('Customers.csv')
 
 df = pd.DataFrame(df)
 
